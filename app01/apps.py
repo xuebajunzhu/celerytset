cdfc19fa9eea5d9e47e3e6f9052db46aa50d895e
xuebajunzhu <52723852+xuebajunzhu@users.noreply.github.com>
2020-02-12 14:48:21 +0800

    first commit2

--- app01/apps.py
+++ app01/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class App01Config(AppConfig):
     name = 'app01'
+    def ready(self):
+        from django.utils.module_loading import autodiscover_modules
+        autodiscover_modules("xx")  #会自动执行各个app的xx文件
